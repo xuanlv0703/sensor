a4dc21a0d0e03ec94e1dba686f095d9f68373c60
Xuan Le <levanxuan@gmail.com>
2016-02-24 14:18:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,20 @@ var client = mqtt.connect('tcp://172.16.0.69:1883', options);
 var app = express();
 app.io = require('socket.io')();
 
+//  OpenShift sample Node application
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+app.listen(port, ip);
+console.log('Server running on ' + ip + ':' + port);
+//end config
+
 //mqtt
 client.on('connect', function() {
     client.subscribe('workplace');
