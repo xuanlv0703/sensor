a958a4bfa165f524287fce7f5177680a8074d687
Xuan Le <levanxuan@gmail.com>
2016-02-24 14:47:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,10 @@ var mysql = require('mysql');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-//mqtt
-var mqtt = require('mqtt');
-var options = {
-    username: "admin",
-    password: "admin"
-};
-var client = mqtt.connect('tcp://172.16.0.69:1883', options);
+
 
 
 var app = express();
-app.io = require('socket.io')();
 
 //  OpenShift sample Node application
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3002;
@@ -29,80 +22,11 @@ app.use(function(err, req, res, next){
   res.status(500).send('Something bad happened!');
 });
 
+app.get('/', function (req, res) {
+    res.send('abc');
+});
+
 app.listen(port, ip);
 console.log('Server running on ' + ip + ':' + port);
 //end config
 
-//mqtt
-client.on('connect', function() {
-    client.subscribe('workplace');
-});
- 
-client.on('message', function(topic, message) {
-    console.log(message.toString());
-    app.io.emit('message',message.toString());
-    //save to database
-});
-
-// CORS
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, CONNECT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Max-Age', '86400'); // 24 hours
-
-    // Intercept OPTIONS method
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
-
-//app.use(express.static(__dirname+'/public'));
-// uncomment after placing your favicon in /public
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-module.exports = app;
